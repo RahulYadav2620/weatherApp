097e7dbd09887a8a5265cdb2d6b994f851ef1e1f
Honey Makhija <rhlkmr2611@gmail.com>
2023-04-18 17:17:09 -0400

    code comment removed

--- public/script.js
+++ public/script.js
@@ -40,7 +40,6 @@ function onError(error){
 }
 
 function fetchData(){
-    console.log("fetchdata test");
     infoTxt.innerText = "Getting weather details.";
     infoTxt.classList.add("pending");
     fetch(api).then(res => res.json()).then(result => weatherDetails(result)).catch(() =>{
